edf36b5d870ad63e449e64feaf114dc677e9925c
Ryan <straightwind0726@gmail.com>
2023-06-13 14:49:13 +0900

    [Test] Git Test

--- src/pages/UpComing.js
+++ src/pages/UpComing.js
@@ -1,13 +1,6 @@
 import React from "react";
-import styled from "styled-components";
 import NowPlayingCard from "../components/NowPlayingCard";
-
-export const PageContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(240px, auto);
-  grid-gap: 8px;
-`;
+import { PageContainer } from "./NowPlaying";
 
 export default function UpComing({ upComingMovies }) {
   return (
